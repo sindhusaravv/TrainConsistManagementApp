7834ec75575874c92e995e51a7804770f0dd2608
sindhusaravv <sindhusaravanan06@gmail.com>
2026-03-25 10:58:27 +0530

    UC1:initalize train and display

---
+++ src/TrainConsistManagementApp.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+ class TrainConsistApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("Train Consist Management App ");
+
+        List<String> trainConsist = new ArrayList<>();
+
+        System.out.println("Initial Bogie Count: " + trainConsist.size());
+    }
+}
